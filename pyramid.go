f5684726a72925fcf51459f73531c58ca9dfe875
Splat137 <53410446+Splat137@users.noreply.github.com>
2024-01-01 13:26:23 +0100

    Add files via upload

---
+++ pyramid.go
@@ -0,0 +1,34 @@
+package kata
+
+func LongestSlideDown(pyramid [][]int) int {
+	if pyramid == nil || len(pyramid) == 0 {
+		return 0
+	}
+	if len(pyramid) == 1 {
+		return pyramid[0][0]
+	}
+
+	max := 0
+
+	for i := len(pyramid) - 2; i > 0; i-- {
+		for j := 0; j < len(pyramid[i]); j++ {
+			max = 0
+			if pyramid[i+1][j] > pyramid[i+1][j+1] {
+				max = pyramid[i+1][j]
+			}
+			if pyramid[i+1][j] <= pyramid[i+1][j+1] {
+				max = pyramid[i+1][j+1]
+			}
+			pyramid[i][j] += max
+		}
+	}
+
+	if pyramid[1][0] > pyramid[1][1] {
+		return pyramid[1][0] + pyramid[0][0]
+	}
+	if pyramid[1][1] >= pyramid[1][0] {
+		return pyramid[1][1] + pyramid[0][0]
+	}
+
+	return 0
+}
