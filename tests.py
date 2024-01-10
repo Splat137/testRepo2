f617e1676601d15fa671c2cbffbbfe3e6c86eb15
Splat137 <53410446+Splat137@users.noreply.github.com>
2024-01-10 08:53:00 +0100

    more tests

--- tests.py
+++ tests.py
@@ -3,8 +3,20 @@ import requests
 import os
 import pytest
 
-def test_addition():
-    assert 1 + 1 == 2
+def test1():
+    assert 1+1 == 2
 
-def test_subtraction():
-    assert 3 - 1 == 2
+def test2():
+    assert 3-1 == 2
+
+def test3():
+    assert True
+
+def test4():
+    assert not False
+
+def test5():
+    assert 2*2 == 4
+
+def test6():
+    assert False
