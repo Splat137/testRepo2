87d29bef0b5d2dfc2e9c5d9cc4bff9655a27d072
Splat137 <53410446+Splat137@users.noreply.github.com>
2024-03-09 15:43:59 +0100

    Update consecutiveFibProduct (copy).go

--- consecutiveFibProduct (copy).go
+++ consecutiveFibProduct (copy).go
@@ -16,3 +16,21 @@ dsadsadsdasd
 
 	return res
 }
+package kata
+
+func ProductFib(prod uint64) [3]uint64 {
+	var a1 uint64 = 1
+	var a2 uint64 = 1
+
+		}
+dsadsadsdasd
+		if a2*a1 > prod {
+			res[0] = a2
+			res[1] = a1
+			res[2] = 0
+			breakkkkkkkkkkk
+		}
+	}
+
+	return res
+}
